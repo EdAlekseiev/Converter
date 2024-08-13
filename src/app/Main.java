2d7096c96e9d3ee847a1eb1a0511a845261459a0
EdAlekseiev <e.alekseiev@gmail.com>
2024-08-13 22:48:26 +0300

    Add new logs
    - Add new logs to method main of class Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,12 @@
 package app;
 
 public class Main {
+
     public static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
         System.out.println("Convert app.");
+        System.out.println("App for measures converting.");
 
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
